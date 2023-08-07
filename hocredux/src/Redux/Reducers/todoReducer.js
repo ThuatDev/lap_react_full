9e8307fc432dd3a8217e8f77d8949a9332570470
teo <phantruongthuatt@gmail.com>
2023-08-07 10:52:10 +0700

    full

---
+++ hocredux/src/Redux/Reducers/todoReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+  todos: [],
+};
+
+export const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "todos/add":
+      return { ...state, todos: state.todos.concat(action.payload) };
+
+    case "todos/fetch":
+      return { ...state, todos: action.payload };
+
+    default:
+      return state;
+  }
+};
+console.log("todoReducer.js");
